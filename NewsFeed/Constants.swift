17ad88d36be9d546cee4f91f9715ed49b4ae21b4
Danyil Marchuk <zemero58@gmail.com>
2023-08-10 16:49:10 +0200

    add background colour constant

---
+++ NewsFeed/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NewsFeed
+//
+//  Created by Данік on 10/08/2023.
+//
+
+import Foundation
+import UIKit
+
+struct K {
+    static let backgroundGray = UIColor(red: 56/255.0, green: 56/255.0, blue: 56/255.0, alpha: 1.0)
+}
